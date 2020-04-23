dab1126c7ac4e1f2f32aa4981329ca86fa2f204b
david <david>
2020-04-24 00:36:51 +0200

    Changed dockerfiles

---
+++ generate_mitm_ca.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# Generate a MITM certificate and key
+echo '{"CN":"CA","key":{"algo":"rsa","size":2048}}' | cfssl gencert -initca - | cfssljson -bare mitm
+mkdir -p /certificates
+cp mitm-key.pem /certificates/mitm.key
+cp mitm.pem /certificates/mitm.crt
+cp mitm.pem /usr/local/share/ca-certificates/mitm.crt
+
+# This directory is for any custom certificates users want to mount
+mkdir -p /certs
+cp /certs/* /usr/local/share/ca-certificates || true
+
+update-ca-certificates --fresh
